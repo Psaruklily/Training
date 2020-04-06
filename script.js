6c7fd4614400db981e5a88c4fe9178c5eacd87f2
Psaruklily <psarukli96@gmail.com>
2020-04-06 13:01:44 +0300

    Usage if..else

--- script.js
+++ script.js
@@ -4,5 +4,14 @@
 let admin = name;
 alert (admin);*/
 
-let userName = prompt ('What is your name?');
-alert (`Your name is ${userName}`);
+/*let userName = prompt ('What is your name?');
+alert (`Your name is ${userName}`);*/
+
+/*4. Используя конструкцию if..else, напишите код, который будет спрашивать: „Какое «официальное» название JavaScript?“
+Если пользователь вводит «ECMAScript», то показать: «Верно!», в противном случае – отобразить: «Не знаете? ECMAScript!»*/
+let officialName = prompt ('What is the official name JavaScript?', '');
+if (officialName == 'ECMAScript') {
+    alert ('True');
+} else {
+    alert ('You do not know? “ECMAScript”!');
+}
