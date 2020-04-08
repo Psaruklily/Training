673c54d9c2cd244db972d4c0387bf816d39c9d87
Psaruklily <psarukli96@gmail.com>
2020-04-08 18:17:16 +0300

    While

--- script.js
+++ script.js
@@ -86,9 +86,15 @@ if (name == 'Адмін') {
 }*/
 
 //При помощи цикла for выведите чётные числа от 2 до 10.
-for (let i = 2; i <= 10; i++) {
+/*for (let i = 2; i <= 10; i++) {
     if (i % 2 !== 0) continue;
     alert (i);
+}*/
+
+let i = 0;
+while (i < 3) {
+    alert (`number ${i}!`);
+    i++;
 }
 
 
