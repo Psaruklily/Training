bff522ee3dd8b5972f2a2c1651d8959270070e06
Psaruklily <psarukli96@gmail.com>
2020-04-15 20:36:37 +0300

    Object. Example 2

--- script.js
+++ script.js
@@ -250,7 +250,7 @@ delete user.name;
 console.log (user);*/
 
 //Напишите функцию isEmpty(obj), которая возвращает true, если у объекта нет свойств, иначе false.
-function isEmpty (schedule) {
+/*function isEmpty (schedule) {
     for (let key in schedule) {
         return false;
     }
@@ -259,7 +259,35 @@ function isEmpty (schedule) {
 let schedule = {};
 //alert( isEmpty(schedule) );
 schedule["8:30"] = "get up";
-alert( isEmpty(schedule) );
+alert( isEmpty(schedule) );*/
+
+//Напишите код для суммирования всех зарплат и сохраните результат в переменной sum.
+/*let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+  };
+  let sum = 0;
+for (let key in salaries) {
+  sum += salaries[key];
+}
+alert(sum);*/
+
+//Создайте функцию multiplyNumeric(obj), которая умножает все числовые свойства объекта obj на 2.
+function multiplyNumeric(obj) {
+    for (let key in obj) {
+      if (typeof obj[key] == 'number') {
+        obj[key] *= 2;
+      }
+    }
+  }
+  let menu = {
+    width: 200,
+    height: 300,
+    title: "My menu"
+  };
+  multiplyNumeric(menu)
+  console.log (menu);
 
 
 
