adab95699be5940797e7336ea624374b98990197
Psaruklily <psarukli96@gmail.com>
2020-04-21 21:27:08 +0300

    Lesson 2

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-let p = document.createElement ('p');
+/*let p = document.createElement ('p');
 document.body.insertBefore (p, document.querySelector ('#out'));
 p.innerHTML = 'I am Lili';
 p.classList.add ('main');
-console.log (p);
+console.log (p);*/
+
+let div4 = document.getElementById ('four4');
+console.log (div4); //Отримали div у консолі;
+
+div4 = document.getElementById ('four4');
+let p = div4.getElementsByTagName ('p');
+console.log (p); // Звернулися до p у div4;
