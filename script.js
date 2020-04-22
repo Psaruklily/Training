13d4e4394cf905be7798757e373ec6c644ec7050
Psaruklily <psarukli96@gmail.com>
2020-04-22 10:00:52 +0300

    Lesson 3

--- script.js
+++ script.js
@@ -4,9 +4,31 @@ p.innerHTML = 'I am Lili';
 p.classList.add ('main');
 console.log (p);*/
 
-let div4 = document.getElementById ('four4');
+/*let div4 = document.getElementById ('four4');
 console.log (div4); //Отримали div у консолі;
 
 div4 = document.getElementById ('four4');
 let p = div4.getElementsByTagName ('p');
-console.log (p); // Звернулися до p у div4;
+console.log (p); // Звернулися до p у div4;*/
+
+
+/*let div = document.querySelector('div');                                                             //Lesson 3
+let child = div.childNodes;  
+console.log (child);  //Повертає вузли, пробіли і переноси;
+console.log (div.firstChild);
+console.log (div.lastChild);
+
+
+let hyper = document.querySelector('a');
+console.log (hyper.parentElement); //Шукаємо батька a;
+
+console.log (hyper.parentElement.nextElementSibling); //Переходимо до наступного div;
+
+console.log (hyper.parentElement.previousElementSibling);*/ // Переходимо до попереднього div;                    
+
+
+
+
+
+
+
