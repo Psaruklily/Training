b156d029d1a635c42f194cd97a080307e019cb27
Psaruklily <psarukli96@gmail.com>
2020-04-25 18:41:02 +0300

    Конструктор

--- script.js
+++ script.js
@@ -373,6 +373,44 @@ alert (sum);*/
     }
 readNumber();*/
 
+//Создайте функцию-конструктор Calculator, который создаёт объекты с тремя методами:
+//read() запрашивает два значения при помощи prompt и сохраняет их значение в свойствах объекта.
+//sum() возвращает сумму введённых свойств.
+//mul() возвращает произведение введённых свойств.
+/* function Calculator () {
+    this.read = function () {
+        this.a = +prompt ('Введіть перше число', '');
+        this.b = +prompt ('Введіть друге число', '');
+    }
+    this.sum = function () {
+        return this.a + this.b;
+    }
+    this.mul = function () {
+        return this.a * this.b;
+    }
+}
+let calculator = new Calculator();
+calculator.read();
+console.log (calculator.sum());
+console.log (calculator.mul()); */
+
+//Напишите функцию-конструктор Accumulator(startingValue).
+//Объект, который она создаёт, должен уметь следующее:
+//Хранить «текущее значение» в свойстве value. Начальное значение устанавливается в аргументе конструктора startingValue.
+//Метод read() использует prompt для получения числа и прибавляет его к свойству value.
+//Свойство value является текущей суммой всего, что ввёл пользователь при вызовах метода read(), с учётом начального значения startingValue.
+/* function Accumulator(startingValue) {
+    this.value = startingValue;
+    this.read = function () {
+        this.value += +prompt ('Введіть перше число', '');
+    }
+}
+let accumulator = new Accumulator(1);
+accumulator.read();
+accumulator.read();
+console.log (accumulator.value); */
+
+
 
 
 
