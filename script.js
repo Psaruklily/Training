2e9d2a4ff116ed727a73110e0325529ef8ce75a4
Psaruklily <psarukli96@gmail.com>
2020-05-03 13:07:41 +0300

    Nav

--- script.js
+++ script.js
@@ -1,19 +1,6 @@
-let link1 = document.querySelector('#link1');
-link1.onclick = function () {
-    this.style.color = 'black';
-} 
-
-let link2 = document.querySelector('#link2');
-link2.onclick = function () {
-    this.style.color = 'black';
-} 
-
-let link3 = document.querySelector('#link3');
-link3.onclick = function () {
-    this.style.color = 'black';
-} 
-
-let link4 = document.querySelector('#link4');
-link4.onclick = function () {
-    this.style.color = 'black';
-} 
+ $(function() {
+    let links = $('a.link').click(function() {
+        links.removeClass('active');
+        $(this).addClass('active');
+    });
+ }); 
