930edf19fb647343db882776825199e094b60db4
Psaruklily <psarukli96@gmail.com>
2020-05-05 12:46:25 +0300

    Fixed header while scrolling

--- script.js
+++ script.js
@@ -4,3 +4,17 @@
         $(this).addClass('active');
     });
  }); 
+
+
+ window.onscroll = function() {myFunction()};
+
+ let header = document.getElementById("myHeader");
+ let sticky = header.offsetTop;
+ 
+ function myFunction() {
+   if (window.pageYOffset > sticky) {
+     header.classList.add("sticky");
+   } else {
+     header.classList.remove("sticky");
+   }
+ }
