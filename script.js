bfb006e1e6f74fa735402d942923178e09c6b291
Psaruklily <psarukli96@gmail.com>
2020-05-17 22:00:22 +0300

    Visibl / invisibl div after onclick on the heart(50%)

--- script.js
+++ script.js
@@ -1,38 +1,105 @@
-/*  $(function() {
-    let links = $('a.link').click(function() {
-        links.removeClass('active');
-        $(this).addClass('active');
-    });
- });  */
+/*GO TO THE MAIN PAGE OF INSTAGRAM*/
+/*get link and contents*/
+let logoInstagram = document.querySelector('.logo-Instagram');
+let container = document.getElementById('container');
+let mainContent = document.getElementById('main-content');
+/*event*/
+logoInstagram.onclick = changeContent;
+
+function changeContent () {
+  container.innerHTML = mainContent.innerHTML;
+}
 
 
-/* 
-  window.onscroll = function() {myFunction()};
 
- let header = document.getElementById("myHeader");
- let sticky = header.offsetTop;
- 
- function myFunction() {
-   if (window.pageYOffset > sticky) {
-     header.classList.add("sticky");
-   } else {
-     header.classList.remove("sticky");
-   }
- }  */
+/*GO TO THE MY PROFILE*/
+/*get link and contents*/
+let iconUser = document.querySelector('.icon-user');
+let aboutUser = document.getElementById('about-user');
+/*event*/
+iconUser.onclick = goToProfile;
 
-/*  $(function(){
-  $("#includedContent").load("index1.html"); 
-}); */
+function goToProfile () {
+  container.innerHTML = aboutUser.innerHTML;
+}
+
+
+
+// EVENTS ON THE HEART
+let heart = document.getElementsByClassName('far fa-heart')[0];
+console.log (heart);
+
+heart.addEventListener ('click', fillHeart); //FILLING OG THE HEARTS BY THE COLOR
+heart.addEventListener ('click', openBox);  // Visibl / invisibl div after onclick on the heart 
 
  function fillHeart(icon) {
+  this.classList.toggle("fas");
+}  
+
+ function openBox(box) {
+  display = document.getElementById ('box').style.display;
+  if (display == "none") {
+    document.getElementById ('box').style.display = "block";
+  } else {
+    document.getElementById ('box').style.display = "none";
+  }
+} 
+
+
+ window.addEventListener('mouseup', function(event){
+  let box = this.document.getElementById('box');
+  if(event.target !== box) {
+    box.style.display = "none";
+  }
+}); 
+
+
+/*   window.onload = function(){
+  let box1 = document.getElementById('box');
+  document.onclick = function(e){
+     if(e.target.id !== 'box'){
+        box1.style.display = 'none';
+     }
+  };
+};  */
+
+
+
+
+/*FILLING OG THE HEARTS BY THE COLOR (like-heart)*/
+
+function fillLikeHeart(icon) {
   icon.classList.toggle("fas");
 } 
 
 
-
-function enableBtn() {
+/*ENABLED / DISABLED BUTTON IN THE COMMENT (after focus textarea)*/
+/* function enableBtn() {
   document.getElementById("myBtn").disabled = false;
+}  */
+
+
+
+/*ENABLED / DISABLED BUTTON IN THE COMMENT (after entered text into textarea)*/
+ function IsEmpty() {
+  let textarea = document.getElementById('text');
+  let button = document.getElementById('button');
+  if (textarea.value !== "") {
+    if (button.classList.contains("disabled")) {
+      button.classList.remove("disabled");
+    }
+  } else {
+    button.classList.add("disabled");
+  }
 } 
+
+
+
+ 
+
+
+
+
 
 
 
