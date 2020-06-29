6125a3ac55f7bc0c5ffca4912a1bd51eed4c4309
Psaruklily <psarukli96@gmail.com>
2020-06-29 20:41:47 +0300

    Random images (url)

--- script.js
+++ script.js
@@ -1,45 +1,61 @@
 //let img = document.querySelector('.image');
 
-let container = document.querySelector('#div');
+/* let container = document.querySelector('#div');
 async function getRandomImage() {
     let response = await fetch('https://random.dog/woof.json?fbclid=IwAR03VGYezarr3RFjvKuZ6TGzqhEv3WtK6n60FVTpoLHsKHNj6b4liCQTqAM');
-    //console.log(response);
     let result = await response.json();
     console.log(result);
     let image = document.createElement('img');
     image.src = result['url'];
     container.appendChild(image);
 }
+
+getRandomImage(); */
+
+/* for (let i = 0; i < 40; i++) {
+    getRandomImage();
+}*/
+
+
+let container = document.querySelector('#div');
+let result;
+let arrayUrl = [];
+async function getRandomImage() {
+    let response = await fetch('https://random.dog/woof.json?fbclid=IwAR03VGYezarr3RFjvKuZ6TGzqhEv3WtK6n60FVTpoLHsKHNj6b4liCQTqAM')
+        .then(function(response) {
+            result = response.json();
+            //console.log(result);
+            return result;
+        })
+        .then(function(result) {
+            let image = document.createElement('img');
+            image.src = result['url'];
+            // console.log(result['url']);
+            container.appendChild(image);
+            return result['url'];
+        })
+        .then(function(result) {
+            //console.log(result);
+            if (!result.includes('mp4')) {
+                arrayUrl.push(result);
+                //console.log(arrayUrl);
+                console.log(result + '   no mp4');
+            }
+
+        })
+}
+
 //getRandomImage();
-for (let i = 0; i < 40; i++) {
+for (let i = 0; i < 10; i++) {
     getRandomImage();
 }
 
 
-
-/* async function getRandomImage() {
-    let response = await fetch('https://random.dog/woof.json?fbclid=IwAR03VGYezarr3RFjvKuZ6TGzqhEv3WtK6n60FVTpoLHsKHNj6b4liCQTqAM');
-    //console.log(response);
-    let result = await response.json();
-    console.log(result);
-    let img = document.createElement("img");
-    img.src = result['url'];
-    let container = document.getElementById("container");
-    container.appendChild(img);
+/* function url(string) {
+    if (string.includes('mp4')) {
+        return 'true';
+    } else {
+        return 'false';
+    }
 } */
-//getRandomImage(); 
-
-/* console.log(container);
-
-function getRandomImage() {
-    let image = document.createElement('img');
-    image.src = 'https://random.dog/945e599c-3a1e-44d5-a23e-d6acc2ca47a5.jpg';
-
-    console.log(container);
-    container.appendChild(image);
-
-}
-for (let i = 0; i < 2; i++) {
-    getRandomImage();
-} */
-//getRandomImage();
+//console.log(url('https://random.dog/fa68ed75-f7e0-44eb-a39b-408b984d2f90.jpg'));
