1e7c6d69e1ba2f9cfb68aa0d8a91ff03c16db5eb
Psaruklily <psarukli96@gmail.com>
2020-07-15 15:10:45 +0300

    Audit url

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ getRandomImage(); */
 
 //--------------------------------------------------------------------------------------------------------------------
 //Та ж сама функція з використанням замикання
-function getRandomImage() {
+/* function getRandomImage() {
     let container = document.querySelector('#div');
     return async function() {
         let response = await fetch('https://random.dog/woof.json?fbclid=IwAR03VGYezarr3RFjvKuZ6TGzqhEv3WtK6n60FVTpoLHsKHNj6b4liCQTqAM');
@@ -33,7 +33,7 @@ const randomImage = getRandomImage();
 
 for (let i = 0; i < 40; i++) {
     randomImage();
-}
+} */
 
 //--------------------------------------------------------------------
 
@@ -78,3 +78,77 @@ for (let i = 0; i < 10; i++) {
     }
 } */
 //console.log(url('https://random.dog/fa68ed75-f7e0-44eb-a39b-408b984d2f90.jpg'));
+
+//-----------------------------------------------------------------------------------------
+
+/* function getRandomImage() {
+    let container = document.querySelector('#div');
+    return async function() {
+        let response = await fetch('https://random.dog/woof.json?fbclid=IwAR03VGYezarr3RFjvKuZ6TGzqhEv3WtK6n60FVTpoLHsKHNj6b4liCQTqAM');
+        let result = await response.json();
+        console.log(result);
+        return result;
+    }
+}
+const randomImage = getRandomImage()();
+
+
+function auditUrl(fn) {
+    //console.log(`this is a string ${string}`);
+    if (string.includes('mp4')) {
+        return 'true';
+    } else {
+        return 'false';
+    }
+}
+//auditUrl();
+auditUrl(getRandomImage); */
+
+//----------------------------------------------------------------------------
+
+let container = document.querySelector('#div');
+async function getRandomImage() {
+    let response = await fetch('https://random.dog/woof.json?fbclid=IwAR03VGYezarr3RFjvKuZ6TGzqhEv3WtK6n60FVTpoLHsKHNj6b4liCQTqAM');
+    let result = await response.json();
+    let urlImage = result['url'];
+    console.log(urlImage);
+    return Promise.resolve(urlImage);
+}
+
+//getRandomImage();
+
+
+/* function url() {
+    const stringUrl = getRandomImage();
+    console.log(getRandomImage);
+    //console.log(typeof stringUrl);
+    //console.log(stringUrl);
+    if (stringUrl.indexOf('jpg') > -1) {
+
+        return 'true';
+    } else {
+        return 'false';
+    }
+} */
+
+
+async function url() {
+    await getRandomImage()
+        .then(stringUrl => {
+            return stringUrl;
+        })
+}
+
+function url2() {
+    const stringUrl = url();
+    console.log(typeof stringUrl);
+    console.log(stringUrl);
+    if (stringUrl.indexOf('jpg') > -1) {
+
+        return 'true';
+    } else {
+        return 'false';
+    }
+}
+const auditUrl = url2()
+console.log(auditUrl);
