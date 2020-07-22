b52349ab37bf2915e79d6e755addf8898153bed9
Psaruklily <psarukli96@gmail.com>
2020-07-22 17:42:21 +0300

    Send GET request

--- lesson1/script.js
+++ lesson1/script.js
@@ -1,4 +1,5 @@
-let button = document.querySelector('.button');
+//Відправлення даних форми на сервер
+/* let button = document.querySelector('.button');
 button.onclick = requestPost;
 
 function requestPost() {
@@ -15,4 +16,24 @@ function requestPost() {
         })
         .then(response => response.json())
         .then(json => console.log(json))
+} */
+
+// Відсилання GET запиту
+function getRequest(url) {
+    let xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            getData(this.responseText);
+        }
+    }
+
+    xhr.open('GET', url);
+    xhr.send();
+
+    function getData(data) {
+        console.log(data);
+    }
 }
+const firstRequest = getRequest('https://learn.javascript.ru/xmlhttprequest');
+console.log(firstRequest);
