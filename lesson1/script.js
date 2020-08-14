a7146fdf3657e86bd08146c79cda540fb2a80bd5
Psaruklily <psarukli96@gmail.com>
2020-08-14 11:08:22 +0300

    'CleantTheCodAboutCOVID'

--- lesson1/script.js
+++ lesson1/script.js
@@ -1,21 +1,14 @@
- let dataWorld = [];
+ let covidWorld = [];
 
  function COVID(callback) {
      let response = getInfoAboutCOVID();
      response.then(data => {
-
-             //console.log(data)
-             //outputDataCOVID(data);
-             //let partOfCountries = data.Countries.slice(0, 5);
-             callback(data);
-             //return partOfCountries;
-         })
-         //.then(data => outputTable(data))
+         callback(data);
+     });
  }
 
  COVID(outputDataCOVID);
  COVID(outputTable);
-
 
  function outputDataCOVID(data) {
      let paragraph = document.querySelectorAll('p');
@@ -24,26 +17,28 @@
      paragraph[2].innerHTML = data.Global.TotalRecovered;
  }
 
-
-
  //------------------TABLE
  let table = document.querySelector('#covid');
 
  function outputTable(data) {
      let partOfCountries = data.Countries.slice(0, 5);
-
-     partOfCountries.forEach((country, index) => {
+     covidWorld = partOfCountries;
+     covidWorld.forEach((country, index) => {
          let tr = document.createElement('tr');
          table.appendChild(tr);
 
-         maketCovid(index + 1, tr);
-         maketCovid(country.Country, tr)
-         maketCovid(country.TotalConfirmed, tr)
-         maketCovid(country.NewConfirmed, tr)
-         maketCovid(country.TotalDeaths, tr)
-         maketCovid(country.NewDeaths, tr)
-         maketCovid(country.TotalRecovered, tr)
+         createRow(index, tr, country);
      });
+ }
+
+ function createRow(index2, tr, country) {
+     maketCovid(index2 + 1, tr);
+     maketCovid(country.Country, tr)
+     maketCovid(country.TotalConfirmed, tr)
+     maketCovid(country.NewConfirmed, tr)
+     maketCovid(country.TotalDeaths, tr)
+     maketCovid(country.NewDeaths, tr)
+     maketCovid(country.TotalRecovered, tr)
  }
 
  function maketCovid(text, tr) {
@@ -59,38 +54,21 @@
  //------------------------------------------------------FILTER BUTTON
  let btnFilter = document.querySelector('.filter');
  btnFilter.onclick = function() {
-     COVID(filterDate);
+     filterDate();
  }
 
-
- function filterDate(data) {
-     let partOfCountries = data.Countries.slice(0, 5);
-     let insedent = partOfCountries.sort(function(a, b) {
+ function filterDate() {
+     let insedent = covidWorld.sort(function(a, b) {
          return a.TotalConfirmed - b.TotalConfirmed;
      });
 
-
-     table.innerHTML = '';
-
-     /*   $(document).ready(function() {
-           $("table").find("tr:gt(0)").remove();
-       }); */
-
-     //$('table').children('tr:not(:first)').remove();
-
-
+     $('table').children('tr').remove();
 
      insedent.forEach((country, index) => {
          let tr = document.createElement('tr');
          table.appendChild(tr);
 
-         maketCovid(index + 1, tr);
-         maketCovid(country.Country, tr)
-         maketCovid(country.TotalConfirmed, tr)
-         maketCovid(country.NewConfirmed, tr)
-         maketCovid(country.TotalDeaths, tr)
-         maketCovid(country.NewDeaths, tr)
-         maketCovid(country.TotalRecovered, tr)
+         createRow(index, tr, country);
      });
 
  }
