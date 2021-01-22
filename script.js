4ace507d43d4176f479c9c6681633deb9d448023
Psaruklily <psarukli96@gmail.com>
2021-01-22 11:51:18 +0300

    Pagination by buttons

--- script.js
+++ script.js
@@ -1,333 +1,102 @@
-/* let container = document.querySelector('#div');
-async function getRandomImage() {
-    let response = await fetch('https://random.dog/woof.json?fbclid=IwAR03VGYezarr3RFjvKuZ6TGzqhEv3WtK6n60FVTpoLHsKHNj6b4liCQTqAM');
-    let result = await response.json();
-    console.log(result);
-    let image = document.createElement('img');
-    image.src = result['url'];
-    container.appendChild(image);
-}
+let table = document.querySelector('table').lastElementChild;
+console.log(table)
 
-getRandomImage(); */
+let navigation = document.querySelector('.navigation');
+let prev = navigation.querySelector('.prev');
+let next = navigation.querySelector('.next');
+let btnNum = navigation.querySelector('ul');
 
-/* for (let i = 0; i < 40; i++) {
-    getRandomImage();
-}*/
-
-
-//--------------------------------------------------------------------------------------------------------------------
-//Та ж сама функція з використанням замикання
-/* function getRandomImage() {
-    let container = document.querySelector('#div');
-    return async function() {
-        let response = await fetch('https://random.dog/woof.json?fbclid=IwAR03VGYezarr3RFjvKuZ6TGzqhEv3WtK6n60FVTpoLHsKHNj6b4liCQTqAM');
-        let result = await response.json();
-        console.log(result);
-        let image = document.createElement('img');
-        image.src = result['url'];
-        container.appendChild(image);
-    }
-}
-
-const randomImage = getRandomImage();
-
-for (let i = 0; i < 40; i++) {
-    randomImage();
-} */
-
-//--------------------------------------------------------------------
-
-/* let container = document.querySelector('#div');
-let result;
-let arrayUrl = [];
-async function getRandomImage() {
-    let response = await fetch('https://random.dog/woof.json?fbclid=IwAR03VGYezarr3RFjvKuZ6TGzqhEv3WtK6n60FVTpoLHsKHNj6b4liCQTqAM')
-        .then(function(response) {
-            result = response.json();
-            //console.log(result);
-            return result;
-        })
-        .then(function(result) {
-            let image = document.createElement('img');
-            image.src = result['url'];
-            // console.log(result['url']);
-            container.appendChild(image);
-            return result['url'];
-        })
-        .then(function(result) {
-            //console.log(result);
-            if (!result.includes('mp4')) {
-                arrayUrl.push(result);
-                //console.log(arrayUrl);
-                console.log(result + '   no mp4');
-            }
-
-        })
-}
-
-for (let i = 0; i < 10; i++) {
-    getRandomImage();
-} */
-
-
-/* function url(string) {
-    if (string.includes('mp4')) {
-        return 'true';
-    } else {
-        return 'false';
-    }
-} */
-//console.log(url('https://random.dog/fa68ed75-f7e0-44eb-a39b-408b984d2f90.jpg'));
-
-//-----------------------------------------------------------------------------------------
-
-/* function getRandomImage() {
-    let container = document.querySelector('#div');
-    return async function() {
-        let response = await fetch('https://random.dog/woof.json?fbclid=IwAR03VGYezarr3RFjvKuZ6TGzqhEv3WtK6n60FVTpoLHsKHNj6b4liCQTqAM');
-        let result = await response.json();
-        console.log(result);
-        return result;
-    }
-}
-const randomImage = getRandomImage()();
-
-
-function auditUrl(fn) {
-    //console.log(`this is a string ${string}`);
-    if (string.includes('mp4')) {
-        return 'true';
-    } else {
-        return 'false';
-    }
-}
-//auditUrl();
-auditUrl(getRandomImage); */
-
-//----------------------------------------------------------------------------
-
-/* let container = document.querySelector('#div');
-async function getRandomImage() {
-    let response = await fetch('https://random.dog/woof.json?fbclid=IwAR03VGYezarr3RFjvKuZ6TGzqhEv3WtK6n60FVTpoLHsKHNj6b4liCQTqAM');
-    let result = await response.json();
-    let urlImage = result['url'];
-    console.log(urlImage);
-    return Promise.resolve(urlImage);
-}
- */
-//getRandomImage();
-
-
-/* function url() {
-    const stringUrl = getRandomImage();
-    console.log(getRandomImage);
-    //console.log(typeof stringUrl);
-    //console.log(stringUrl);
-    if (stringUrl.indexOf('jpg') > -1) {
-
-        return 'true';
-    } else {
-        return 'false';
-    }
-} */
-
-
-/* async function url() {
-    await getRandomImage()
-        .then(stringUrl => {
-            return stringUrl;
-        })
-}
-
-function url2() {
-    const stringUrl = url();
-    console.log(typeof stringUrl);
-    console.log(stringUrl);
-    if (stringUrl.indexOf('jpg') > -1) {
-
-        return 'true';
-    } else {
-        return 'false';
-    }
-}
-const auditUrl = url2()
-console.log(auditUrl); */
-
-
-
-/////////////////////////Callback
-
-/* const getUrl = function(url, callback) {
-    let request = new XMLHttpRequest();
-    request.onreadystatechange = function() {
-        if (request.readyState == 4 && request.status == 200) {
-            callback(request.responseText);
-        }
-    };
-    request.open('GET', url);
-    request.send();
-}
-
-function mycallback(data) {
-    console.log(data);
-    newData = JSON.parse(data);
-    console.log(newData['url']);
-    return newData['url'];
-}
-
-getUrl('https://random.dog/woof.json?fbclid=IwAR03VGYezarr3RFjvKuZ6TGzqhEv3WtK6n60FVTpoLHsKHNj6b4liCQTqAM', mycallback); */
-
-
-
-
-
-// PROMISE
-/* 
-async function getRandomUrl() {
-    const url = 'https://random.dog/woof.json?fbclid=IwAR03VGYezarr3RFjvKuZ6TGzqhEv3WtK6n60FVTpoLHsKHNj6b4liCQTqAM';
-    let promise = await fetch(url);
-    let urlImage = await promise.json();
-    //console.log(stringUrl);
-    console.log(urlImage['url']);
-    return urlImage['url'];
-}
-
-getRandomUrl()
-    .then(string => {
-        if (!string.includes('jpg')) {
-            getRandomUrl();
-        }
-        return string;
-    }) */
-
-
-//-------------------------------------------------------------------------
-//      FINISHED!!!!!
-// container = document.querySelector('#div');
-// async function getRandomUrl(callback) {
-//     const url = 'https://random.dog/woof.json?fbclid=IwAR03VGYezarr3RFjvKuZ6TGzqhEv3WtK6n60FVTpoLHsKHNj6b4liCQTqAM';
-//     let promise = await fetch(url);
-//     let urlImage = await promise.json();
-//     let stringUrl = urlImage['url'];
-//     let image = document.createElement('img');
-//     image.src = stringUrl;
-//     container.appendChild(image);
-//     callback(stringUrl, image);
-// }
-
-// function auditUrl(stringUrl, image) {
-//     console.log(stringUrl);
-//     if (stringUrl.includes('jpg')) {
-//         image.style.display = 'block';
-//     } else {
-//         image.style.display = 'none';
-//         getRandomUrl(auditUrl);
-
-//     }
-// }
-
-// for (let i = 0; i < 40; i++) {
-//     getRandomUrl(auditUrl);
-// }
-
-//====================================================================================  ПІДГРУЗКА КАРТИНОК ПРИ СКРОЛІ start
-
-// let container = document.querySelector('.container');
-
-// fetch('https://randomuser.me/api/?inc=gender,name,picture,location&results=5')
-//     .then(response => response.json())
-//     .then(result => {
-//         return result.results;
-//     })
-//     .then(arrUsers => {
-
-//         for (let user of arrUsers) {
-//             let image = document.createElement('img');
-//             image.src = '';
-//             image.dataset.url = user.picture.large;
-//             container.appendChild(image) // Резервую місце для кожної каритнки
-//         }
-
-//         let images = document.querySelectorAll('.container>*');
-
-//         let options = {
-//             root: null,
-//             rootMargin: '0px',
-//             threshold: 0.1
-//         }
-
-//         function handlerImg(myImg, observer) {
-//             console.log('Data: ', myImg) //Отримуємо масив об'єктів (спостережувальні картинки із їх властивостями, де зазначено певні зміни)
-//             myImg.forEach(currentImg => {
-//                 console.log(currentImg.intersectionRatio);
-//                 if (currentImg.intersectionRatio > 0) {
-//                     loadImage(currentImg.target)
-//                 }
-//             })
-//         }
-
-//         function loadImage(image) {
-//             image.src = image.dataset.url;
-//         }
-
-//         let observer = new IntersectionObserver(handlerImg, options);
-
-//         images.forEach(image => {
-//             observer.observe(image);
-//         })
-//     })
-
-//====================================================================================  ПІДГРУЗКА КАРТИНОК ПРИ СКРОЛІ end
-
-//===================================================================================ПАГІНАЦІЯ ПРИ СКРОЛІ СТОРІНКИ start
-
-// 1. Отримати дані від backendy
-// 2. Запушити отримані дані у новий пустий масив
-// 3. Вивести дані першої сторінки одразу після завантаження сайту
-// 4. Коли я доскролюю до останнього елементу масиву, робити новий запит 
-
-let isRequest = false;
-let containerForImage = document.querySelector('.container-for-image');
-let indexPage = 0;
+let currentPage = 0;
 
 async function getData() {
-    isRequest = true;
-    let promise = await fetch(`https://api.instantwebtools.net/v1/passenger?page=${indexPage}&size=10`);
+    let promise = await fetch(`https://api.instantwebtools.net/v1/passenger?page=${currentPage}&size=10`);
     let result = await promise.json();
-
-    indexPage++;
-    isRequest = false;
     return result;
 }
 
-async function outputData() {
-    let data = await getData()
-    let array = data.data;
-    console.log(array)
+buttons()
 
-    for (let item of array) {
-        let image = document.createElement('img');
-        image.src = item.airline.logo;
-        image.classList.add('for-image');
-        containerForImage.appendChild(image);
+//Заповнюю комірки даними
+async function outputData() {
+    let array = await getData();
+    let arrWithCompan = array.data;
+    console.log(arrWithCompan);
+    table.innerHTML = '';
+    for (let item of arrWithCompan) {
+        let tr = document.createElement('tr');
+        table.appendChild(tr);
+        fillTd(item.name, tr);
+        fillTd(item.trips, tr);
+        fillTd(item._id, tr);
     }
 }
 
 outputData()
 
-window.addEventListener('scroll', function() {
-    let window_height = window.innerHeight;
-    let contentHeight = containerForImage.offsetHeight;
-    let yOffset = window.pageYOffset;
-    // console.log('window_height', window_height);
-    // console.log('contentHeight', contentHeight);
-    // console.log('yOffset', yOffset);
-    // console.log('=============================')
-
-    if (yOffset + window_height + 100 >= contentHeight && !isRequest) {
-        console.log('end')
-        outputData()
+//Виводжу кнопки навігації
+async function buttons() {
+    let data = await getData();
+    let pages = data.totalPages;
+    for (let i = 1; i < pages; i++) {
+        let li = document.createElement('li');
+        li.innerHTML = i;
+        if (i == 1) {
+            li.classList.add('activeCl');
+        }
+        btnNum.appendChild(li);
     }
-})
+}
 
-//===================================================================================ПАГІНАЦІЯ ПРИ СКРОЛІ СТОРІНКИ end
+function fillTd(text, tr) {
+    let td = document.createElement('td');
+    td.innerText = text;
+    tr.appendChild(td);
+}
+
+//функція слухач подій
+function listener() {
+    btnNum.addEventListener('click', function(event) {
+        let target = event.target;
+        console.log(target)
+        currentPage = target.innerHTML - 1;
+        outputData();
+        let active = document.querySelector(".activeCl");
+        if (active !== null) {
+            active.classList.remove('activeCl');
+        }
+        target.classList.add('activeCl');
+
+        if (target.innerHTML == '1') {
+            prev.style.visibility = 'hidden';
+        } else {
+            prev.style.visibility = 'visible';
+        }
+
+        if (target.innerHTML == '408') {
+            next.style.visibility = 'hidden';
+        } else {
+            next.style.visibility = 'visible';
+        }
+    })
+
+    prev.addEventListener('click', function() {
+        currentPage = currentPage - 1;
+        outputData();
+    })
+
+    next.addEventListener('click', function() {
+        currentPage = currentPage + 1;
+        outputData();
+        let active = document.querySelector(".activeCl");
+        console.log('active', active)
+
+        // console.log('dont', dontActive)
+        if (active !== null) {
+            active.classList.remove('activeCl');
+
+        }
+        active.nextSibling.classList.add('active')
+    })
+}
+
+listener()
