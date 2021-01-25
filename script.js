4dba1b6f1520261b8bc3e4aab8a067e9edda59ed
Psaruklily <psarukli96@gmail.com>
2021-01-25 20:34:35 +0300

    Finished todoList

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ const messageForm = document.querySelector('form');
 const messageInput = document.querySelector('[name = "message"]');
 const resetBtn = document.querySelector('.btn-dark');
 const ul = document.querySelector('.list ul');
+const saveBtn = document.querySelector('.save-btn');
 const updateBtn = document.querySelector('.update-btn');
 const cleanUp = document.querySelector('.сlean-up');
+
 // 1. Якщо у localStorage уже записані дані, тоді присвоїти цій змінній їх, якщо ні - пустий масив;
 let itemsArray = localStorage.getItem('items') ?
     JSON.parse(localStorage.getItem('items')) : []
@@ -16,47 +18,53 @@ const liMaker = text => {
     let mainDiv = document.createElement('div');
     let task = document.createElement('div');
     let divForButn = document.createElement('div');
-    let divForChecbox = document.createElement('div'); //==
     let p = document.createElement('p');
     p.textContent = text;
-    p.classList.add('task-text') //===
+    p.classList.add('task-text')
     mainDiv.classList.add('main-div-li')
-    let checkbox = document.createElement('input') //==
+    let checkbox = document.createElement('input')
     checkbox.type = 'checkbox';
     checkbox.classList.add('checkbox')
+    checkbox.onclick = function() {
+        if (checkbox.checked == true) {
+            p.style.textDecoration = 'line-through'
+        } else {
+            p.style.textDecoration = 'none'
+        }
+    }
 
-    task.classList.add('task') //===
+    task.classList.add('task')
     task.appendChild(checkbox)
     task.appendChild(p);
     mainDiv.appendChild(task);
 
-    let updateBtn = document.createElement('button');
-    updateBtn.textContent = 'Update'
-    updateBtn.classList.add('update');
+    let update = document.createElement('button');
+    update.innerHTML = '<i class="fas fa-pencil-alt"></i>';
+    update.style.color = '#fff';
+    update.classList.add('update-small');
 
-    let cancelBtn = document.createElement('button');
-    cancelBtn.textContent = 'Cancel';
-    cancelBtn.classList.add('delete');
+    let cancel = document.createElement('button');
+    cancel.innerHTML = '<i class="fas fa-trash-alt"></i>';
+    cancel.style.color = '#fff';
+    cancel.classList.add('delete-small');
 
-    divForButn.appendChild(updateBtn)
-    divForButn.appendChild(cancelBtn)
+    divForButn.appendChild(update)
+    divForButn.appendChild(cancel)
 
     mainDiv.appendChild(divForButn)
 
     li.appendChild(mainDiv)
     ul.appendChild(li);
-    let hr = document.createElement('hr') ///==
+    let hr = document.createElement('hr')
     ul.appendChild(hr)
 
     if (itemsArray.length > 0) {
         resetBtn.classList.remove('hide');
     }
 
-    let update = mainDiv.lastElementChild.firstElementChild;
-    let cancel = mainDiv.lastElementChild.lastElementChild;
-
     mainDiv.addEventListener('click', function(event) {
         if (event.target === cancel) {
+            console.log('cancel')
             itemsArray = itemsArray.filter(task => task !== text)
             localStorage.setItem('items', JSON.stringify(itemsArray));
             this.parentNode.parentNode.removeChild(this.parentNode);
@@ -64,6 +72,8 @@ const liMaker = text => {
                 resetBtn.classList.add('hide');
             }
         } else if (event.target === update) {
+            saveBtn.classList.add('display-none');
+            updateBtn.classList.remove('display-none');
             messageInput.value = text;
             let index = itemsArray.findIndex(el => el === messageInput.value);
             updateLocalStorage(index, p)
@@ -78,14 +88,21 @@ messageForm.addEventListener('submit', function(e) {
     itemsArray.push(messageInput.value);
     localStorage.setItem('items', JSON.stringify(itemsArray));
     liMaker(messageInput.value);
-    console.log('create item')
+    messageInput.value = '';
+})
+
+saveBtn.addEventListener('click', function(e) {
+    e.preventDefault();
+    if (messageInput.value === '') return false;
+    itemsArray.push(messageInput.value);
+    localStorage.setItem('items', JSON.stringify(itemsArray));
+    liMaker(messageInput.value);
     messageInput.value = '';
 })
 
 // Одразу після завантаження сторінки виводжу items
 itemsArray.forEach(item => {
     liMaker(item);
-    console.log('output on page out')
 });
 
 resetBtn.addEventListener('click', function() {
@@ -100,6 +117,8 @@ resetBtn.addEventListener('click', function() {
 function updateLocalStorage(indexItem) {
 
     updateBtn.addEventListener('click', function() {
+        updateBtn.classList.add('display-none');
+        saveBtn.classList.remove('display-none')
         itemsArray[indexItem] = messageInput.value;
         localStorage.setItem('items', JSON.stringify(itemsArray));
 
@@ -114,3 +133,5 @@ function updateLocalStorage(indexItem) {
 cleanUp.addEventListener('click', () => {
     messageInput.value = null;
 })
+
+console.log(new Date())
